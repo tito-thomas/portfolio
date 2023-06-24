aff069c0b3c067cba9ff9d837b1d508cf6afe2f3
Oluwatito Thomas <thomasoo@cardiff.ac.uk>
2023-06-24 06:12:16 +0100

    Added second project. Need to fix slideshow bug

--- Scripts/custom/slideshow.js
+++ Scripts/custom/slideshow.js
@@ -1,4 +1,5 @@
 ﻿
+/*
 let counter = 1;
 
 function carousel() {
@@ -20,6 +21,53 @@ function carousel() {
         slide.style.transform = "translateX(" + (-width * counter) + "px)";
         counter++;
     }
-    
+
 }
-setInterval(carousel, 2000)
+setInterval(carousel, 2000)*/
+//let counter = 1;
+class Slideshow { 
+    constructor(slide, images) {
+        this.slide = slide;
+        this.images = images;
+        this.counter = 1;
+    }
+    
+    
+    carousel() {
+        
+        let my_interval = setInterval(() => {
+            const width = this.images[0].clientWidth;
+            
+            if (this.counter == this.images.length) {
+                console.log("end");
+                this.counter = 0;
+                this.slide.style.transition = "none";
+                //think of translate values as coordinates not the amount that it moves the element 
+                this.slide.style.transform = "translateX(" + (-width * this.counter) + "px)";
+                //this.counter++;
+                //this.carousel();
+            }
+            else {
+                this.slide.style.transition = "transform 0.4s ease-in-out";
+                this.slide.style.transform = "translateX(" + (-width * this.counter) + "px)";
+                //this.counter++;
+            }
+            this.counter++;
+            console.log(this.counter)
+            
+        }, 1000);
+    }
+}
+
+//slide 1
+const slide = document.querySelector(".slider");
+const images = document.querySelectorAll(".slider img");
+const first_slide = new Slideshow(slide, images);
+first_slide.carousel();
+
+//slide 2
+const slide2 = document.querySelector(".slider2");
+const images2 = document.querySelectorAll(".slider2 img");
+const second_slide = new Slideshow(slide2, images2);
+second_slide.carousel();
+
