19100e941ad8c18a6aaa83fdf9b02860372be5b8
Oluwatito Thomas <thomasoo@cardiff.ac.uk>
2023-07-11 19:56:16 +0100

    Added "about me" animation

---
+++ Scripts/custom/animations.js
@@ -0,0 +1,23 @@
+﻿
+
+//About me animation
+
+const tracker = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.style.transition = "transform 0.5s";
+            entry.target.style.transform = "translateX(0)";
+
+        }
+        else if(entry.target.id==="biotext"){
+            entry.target.style.transform = "translateX(-100%)";
+        }
+        else if(entry.target.id==="selfie"){
+            entry.target.style.transform = "translateX(100%)";
+        }
+    });
+});
+
+const bioitems = document.querySelectorAll(".bioitem");
+bioitems.forEach((item) => tracker.observe(item));
