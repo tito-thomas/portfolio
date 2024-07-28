267f72132dcbb216ad0a46d905f57356c797303e
Oluwatito Thomas <thomasoo@cardiff.ac.uk>
2024-07-28 02:02:39 +0100

    small visual changes

--- api/index.js
+++ api/index.js
@@ -5,15 +5,10 @@ const router = require('./router');
 const { exec } = require('child_process');
 
 
-const url = 'http://localhost:3000';
+const url = 'http://localhost:5000';
 
 // app.use(express.static(path.join(__dirname, '/')));
 app.use(express.static(path.join(__dirname, '../public')));
-// app.use('/Content', express.static(path.join(__dirname, '../public/Content')));
-// app.use('/Scripts/custom', express.static(path.join(__dirname, '../public/Scripts/custom')));
-// app.use('/Home', express.static(path.join(__dirname, '../public/Views/Home')));
-// app.use('/Shared', express.static(path.join(__dirname, '../public/Views/Shared')));
-// app.use('/Shared', express.static(path.join(__dirname, '/Shared')));
 app.use('/', router)
 
 console.log("RUNNING")
@@ -21,8 +16,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
     console.log(`Server is running on port 5000`);
 });
-
-export default app
 // exec(`start chrome ${url}`, (error, stdout, stderr) => {
 //   if (error) {
 //     console.error(`Error opening Chrome: ${error}`);
