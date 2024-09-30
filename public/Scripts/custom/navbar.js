e3943f4e38e83911dac432b48e4ede693cc4517e
Tito Thomas <titothomas@hotmail.co.uk>
2024-09-30 19:22:20 +0100

    fixed navbar toggle

--- public/Scripts/custom/navbar.js
+++ public/Scripts/custom/navbar.js
@@ -17,32 +17,23 @@ function hideNavElements() {
 };
 
 function navScrollStyling() {
-    const navbar = document.querySelector("#mynav")
-    navbar.style.transition = "0.4s"
-    if (window.scrollY != 0) {
-        navbar.style.backgroundColor = "rgb(3, 36, 86, 0.85)";
-        navbar.style.backdropFilter = "blur(5px)"
+    if(screen.width > 768){
+        const navbar = document.querySelector("#mynav")
+        navbar.style.transition = "0.4s"
+        if (window.scrollY != 0) {
+            navbar.style.backgroundColor = "rgb(3, 36, 86, 0.85)";
+            navbar.style.backdropFilter = "blur(5px)"
 
-    }
-    else {
-        navbar.style.backgroundColor = "transparent"
-        navbar.style.backdropFilter = ""
+        }
+        else {
+            navbar.style.backgroundColor = "transparent"
+            navbar.style.backdropFilter = ""
+        }
     }
  }
+ window.addEventListener("scroll", navScrollStyling);
 
-//document.addEventListener("DOMContentLoaded", footerDate); 
-window.addEventListener("scroll", navScrollStyling);
-/*function checkHomePage() {
 
-    var url = document.URL.split('/')
-    //check if we are on the home page
-    if (document.URL.includes("Index") || url.length < 5) {
-        const navbar = document.querySelector("#mynav")
-        document.body.style.marginTop = "0"
-        navbar.style.backgroundColor = "transparent"
 
-        window.addEventListener("scroll", homeNavStyling)
-    }
-    
-}*/
+
 
